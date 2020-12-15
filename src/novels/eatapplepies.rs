c5444209d4d65b6a1242ebed03aef43cca9cb52c
BrenoLopes <brenolprimo@gmail.com>
2020-12-15 18:15:33 -0400

    Fixed: removed wordpress metadata from text

--- src/novels/eatapplepies.rs
+++ src/novels/eatapplepies.rs
@@ -66,7 +66,7 @@ impl NovelProvider for EatApplePies
               <h1 class="entry-title">{{title}}</h1>
             </header>
             <div class="entry-content">
-              {{body:*}}
+              <p>{{body}}</p>
             </div>
           </article>
         </div>
@@ -74,25 +74,42 @@ impl NovelProvider for EatApplePies
     "#).expect("Could not load the pattern.");
 
     let matches = pattern.matches(html);
-    let m = &matches[0];
 
     let mut data = String::from("");
 
-    match m.get("title") {
+    match matches[0].get("title") {
       None => {},
       Some(d) => {
         let re = regex::Regex::new(r".+Chapter\s0*(\d+)[\s:–-]+(.+)")
           .unwrap();
         let new_data =
-          format!("{}\n\n",re.replace(d, "Chapter $1: $2").to_string());
+          format!("{}\n\n",re.replace(d, "<h1>Chapter $1: $2</h1>").to_string());
         data.push_str(&new_data);
       }
     }
 
-    match m.get("body") {
-      None => return None,
-      Some(body) => Some(data.push_str(body))
-    };
+    let invalid_html = vec![
+      "No account yet?",
+      "Register",
+      "Username or Email Address",
+      "Password",
+      "Remember Me",
+    ];
+
+    for m in matches {
+      if let Some(m) = m.get("body") {
+        if invalid_html.contains(&&**m) {
+          continue;
+        }
+
+        let paragraph = format!("<p>{}</p>\n", m);
+        data.push_str(&paragraph);
+      }
+    }
+
+    // println!("\nDebug");
+    // println!(data);
+    // println!("Debug\n");
 
     return Some(data);
   }
@@ -102,6 +119,27 @@ impl NovelProvider for EatApplePies
 mod tests {
   use crate::novels::eatapplepies::EatApplePies;
   use crate::novels::providers::NovelProvider;
+  use crate::url;
+  use std::io::Write;
+
+  #[test]
+  fn test_main() {
+    let url1 = "https://eatapplepies.com/tcf-chapter-1/";
+    let url2 = "https://eatapplepies.com/tcf-chapter-2/";
+
+    let html1 = url::download(url1).unwrap();
+    let html2 = url::download(url2).unwrap();
+
+    let parser = EatApplePies::new();
+
+    let chapter1 = parser.get_chapter(&html1).unwrap();
+    let chapter2 = parser.get_chapter(&html2).unwrap();
+
+    let mut f = std::fs::File::create("AFF.html").unwrap();
+    f.write(chapter1.as_bytes()).unwrap();
+    f.write("\n\n".as_bytes()).unwrap();
+    f.write(chapter2.as_bytes()).unwrap();
+  }
 
   #[test]
   fn test_supports_url_simple() {
@@ -210,41 +248,40 @@ mod tests {
     let guinea_pig = EatApplePies::new();
 
     let html = r#"
-    <html>
-      <div id="primary">
-        <div id="content" role="main">
-          <nav id="nav-single">
-            <h3 class="assistive-text">Post navigation</h3>
-            <span class="nav-previous"></span>
-            <span class="nav-next"><a href="https://eatapplepies.com/tcf-chapter-2/" rel="next">Next <span class="meta-nav">&rarr;</span></a></span>
-          </nav><!-- #nav-single -->
-          <article id="post-11" class="post-11 post type-post status-publish format-standard has-post-thumbnail hentry category-tcf">
-            <header class="entry-header">
-              <h1 class="entry-title">Trash of the Count&#8217;s Family &#8211; Chapter 1 &#8211; Prologue</h1>
-              <div class="entry-meta">
-                <span class="sep">Posted on </span><a href="https://eatapplepies.com/tcf-chapter-1/" title="9:24 pm" rel="bookmark"><time class="entry-date updated" datetime="2020-11-03T21:24:24+00:00" pubdate>November 3, 2020</time></a><span class="by-author"> <span class="sep"> by </span> <span class="author vcard"><a class="url fn n" href="https://eatapplepies.com" title="View all posts by admin" rel="author">admin</a></span></span>                                    <span class="sep"> &mdash; </span>
-                <span class="comments-link">
-                <a href="https://eatapplepies.com/tcf-chapter-1/#comments">9 Comments &darr;</a>                    </span>
-              </div><!-- .entry-meta -->
-            </header><!-- .entry-header -->
-            <div class="entry-content">
-              <p>When I opened my eyes, I was inside a novel.</p>
-              <p>[The Birth of a Hero].</p>
-              <p>[The Birth of a Hero] was a novel focused on the adventures of the main character, Choi Han, a high school boy who was transported to a different dimension from Earth, along with the birth of the numerous heroes of the continent.</p>
-            </div>
-          </article>
+      <html>
+        <div id="primary">
+          <div id="content" role="main">
+            <nav id="nav-single">
+              <h3 class="assistive-text">Post navigation</h3>
+              <span class="nav-previous"></span>
+              <span class="nav-next"><a href="https://eatapplepies.com/tcf-chapter-2/" rel="next">Next <span class="meta-nav">&rarr;</span></a></span>
+            </nav><!-- #nav-single -->
+            <article id="post-11" class="post-11 post type-post status-publish format-standard has-post-thumbnail hentry category-tcf">
+              <header class="entry-header">
+                <h1 class="entry-title">Trash of the Count&#8217;s Family &#8211; Chapter 1 &#8211; Prologue</h1>
+                <div class="entry-meta">
+                  <span class="sep">Posted on </span><a href="https://eatapplepies.com/tcf-chapter-1/" title="9:24 pm" rel="bookmark"><time class="entry-date updated" datetime="2020-11-03T21:24:24+00:00" pubdate>November 3, 2020</time></a><span class="by-author"> <span class="sep"> by </span> <span class="author vcard"><a class="url fn n" href="https://eatapplepies.com" title="View all posts by admin" rel="author">admin</a></span></span>                                    <span class="sep"> &mdash; </span>
+                  <span class="comments-link">
+                  <a href="https://eatapplepies.com/tcf-chapter-1/#comments">9 Comments &darr;</a>                    </span>
+                </div><!-- .entry-meta -->
+              </header><!-- .entry-header -->
+              <div class="entry-content">
+                <p>When I opened my eyes, I was inside a novel.</p>
+                <p>[The Birth of a Hero].</p>
+                <p>[The Birth of a Hero] was a novel focused on the adventures of the main character, Choi Han, a high school boy who was transported to a different dimension from Earth, along with the birth of the numerous heroes of the continent.</p>
+              </div>
+            </article>
+          </div>
         </div>
-      </div>
-    </html>
-  "#;
+      </html>
+    "#;
 
     let expected =
-      "Chapter 1: Prologue\n\n<p>When I opened my eyes, I was inside a novel.</p><p>[The Birth of a Hero].</p><p>[The Birth of a Hero] was a novel focused on the adventures of the main character, Choi Han, a high school boy who was transported to a different dimension from Earth, along with the birth of the numerous heroes of the continent.</p>";
+      "<h1>Chapter 1: Prologue</h1>\n\n<p>When I opened my eyes, I was inside a novel.</p>\n<p>[The Birth of a Hero].</p>\n<p>[The Birth of a Hero] was a novel focused on the adventures of the main character, Choi Han, a high school boy who was transported to a different dimension from Earth, along with the birth of the numerous heroes of the continent.</p>\n";
 
     let received = guinea_pig.get_chapter(&html)
       .expect("Should not have any errors in the parsing!");
 
-    // let _ = std::fs::write("ohno.txt", received);
     assert_eq!(expected, received, "This test should pass because both results must return true");
   }
 }
